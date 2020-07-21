486118bb781e2125253a11168e01c2247c0aae05
Thibaud <thibaud29@protonmail.com>
2020-07-21 21:49:15 +0200

    ResourceType

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UserSeeder::class);
         // $this->call(GameSeeder::class);
         $this->call(ProductionBlocSeeder::class);
+        $this->call(ResourceTypeSeeder::class);
     }
 }
