95d5f1eeaebfb9f029b85a721878fd7a2cadb6e8
Thibaud <thibaud29@protonmail.com>
2020-07-23 19:39:08 +0200

    game list

---
+++ tests/Feature/GameTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Game;
+
+class GameTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_games_can_list()
+    {
+        // $now = now
+        $games = factory(Game::class, 3)->create();
+
+        $this->getJson('/api/games')
+             ->assertStatus(200)
+             ->assertJson($games->toArray());
+    }
+}
