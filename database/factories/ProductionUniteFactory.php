59a3428925ce131e916f0db9208fd162438ab1ec
Thibaud <thibaud29@protonmail.com>
2020-07-31 20:15:10 +0200

    update ProductionUniteFactory

--- database/factories/ProductionUniteFactory.php
+++ database/factories/ProductionUniteFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(ProductionUnite::class, function (Faker $faker) {
     return [
         'recipe_id' => 1,
-        'production_bloc_id' => 1,
+        'production_bloc_id' => factory(App\Models\ProductionBloc::class)->create()->id,
         'building_id' => 1,
         'name' => $faker->name,
     ];
